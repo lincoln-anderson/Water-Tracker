ba060f7e4fe88fe100c0c93eed06b22d78f8186d
LincAnderson <36419018+LincAnderson@users.noreply.github.com>
2020-07-01 09:11:14 -0400

    Initial Commit

---
+++ Shared/Water_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Water_TrackerApp.swift
+//  Shared
+//
+//  Created by lincoln anderson on 7/1/20.
+//
+
+import SwiftUI
+
+@main
+struct Water_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
